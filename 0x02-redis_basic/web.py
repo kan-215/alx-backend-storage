6ad6c51e93e538926c98d62b84464b4e891c549b
kan-215 <katamialvin369@gmail.com>
2024-10-23 22:45:11 +0300

    redis

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""implements a web cache"""
+""" web cache"""
 import requests
 import redis
 
@@ -11,17 +11,17 @@ def get_page(url: str) -> str:
     '''implements the cache'''
     key = "count:{}".format(url)
     '''
-    increment the count of how many times url has been visited
+    gives the count of how many times a URL has been visited
     '''
     r.incr(key)
     cached_data = r.get(url)
     '''
-    check if we have data associated with url in cache
+    checks for the  data associated with url in cache
     '''
     if cached_data is not None:
         return cached_data.decode('utf-8')
     '''
-    incase we have no data, query the url and add in cache for 10 seconds
+    if there is no data, query the url and add in cache for 10 seconds
     '''
     resp = requests.get(url)
     info = resp.text
